9d50197141e5214c8e2dd8c6132d9b81d928b8b7
GABRIEL THEMER BRITO <gabriel.tbrito@senacsp.edu.br>
2025-09-04 17:31:06 -0300

    testando a controller dos quartos

---
+++ controllers/QuartoController.php
@@ -0,0 +1,45 @@
+<?php
+    require_once __DIR__ . "/../models/QuartoModel.php";
+
+    class QuartoController{
+
+        public static function create($conn,$data){
+            $resultado = QuartoModel :: create($conn,$data);
+            if($resultado){
+              return jsonResponse(['message' => "Quarto reservado com sucesso"]);
+            }else{
+               return jsonResponse(['message' => "Quarto nao reservado"], 404);
+            }
+        }
+
+        public static function getAll($conn){
+            $listaQuarto = QuartoModel::getAll($conn);
+            return jsonResponse($listaQuarto);
+        }
+
+        public static function getById($conn,$id){
+            $IdQuarto = QuartoModel::getById($conn,$id);
+            return jsonResponse($IdQuarto);
+        }
+
+         public static function delete($conn,$id){
+            $deletado = QuartoModel :: delete($conn,$id);
+            if($deletado){
+              return jsonResponse(['message' => "Deletado com Sucesso"]);
+            }else{
+               return jsonResponse(['message' => "Erro ao Deletar"], 404);
+            }
+        }
+
+        public static function update($conn,$id,$data){
+            $Atualizado = QuartoModel :: update($conn,$id,$data);
+            if($Atualizado){
+              return jsonResponse(['message' => "Quarto atualizado com sucesso"]);
+            }else{
+               return jsonResponse(['message' => "Quarto nao atualizado"], 404);
+            }
+        }
+
+
+    }
+?>
--- teste.php
+++ teste.php
@@ -1,19 +1,36 @@
 <?php
     require_once __DIR__ ."/controllers/AuthController.php";
     require_once __DIR__ ."/controllers/Password.php";
-    require_once __DIR__. "/helpers/token_jwt.php";
+    require_once __DIR__ ."/helpers/token_jwt.php";
+    require_once __DIR__ ."/controllers/QuartoController.php";
 
-    $data = [
-        "email"=>"pamellapereto@gmail.com",
-        "senha"=>"1234"
-    ];
+
+         $data = [
+            "nome"=>"Quarterao",
+            "numero"=> 10,
+            "qnt_casal"=> 2,
+            "qnt_solteiro"=> 2,
+            "preco"=> 500,
+            "disponivel"=> 10
+        ];
+
+     QuartoController :: update ($conn,31,$data);
+
+
+
+
+    // QuartoController :: delete ($conn,21);
+    //QuartoController :: getById ($conn,21);
+    //QuartoController :: getAll ($conn);
+
+    //QuartoController :: create($conn,$data);
 
     //AuthController::login($conn,$data);
 
-    $tokeninvalido = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.KMUFsIDTnFmyG3nMiGM6H9FNFUROf3wh7SmqJp-QV30";
-    $tokenvalido = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJNZXVzaXRlIiwiaWF0IjoxNzU2OTMwNDE1LCJleHAiOjE3NTY5MzQwMTUsInN1YiI6eyJpZCI6NSwibm9tZSI6IlBhbWVsbGEiLCJlbWFpbCI6InBhbWVsbGFwZXJldG9AZ21haWwuY29tIiwiY2FyZ28iOiJjYW1hcmVpcmEifX0.tPiz6TM3C690nlJf9iaqrmKL-92yFJQVvFK_lgtQp1k";
+    //$tokeninvalido = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.KMUFsIDTnFmyG3nMiGM6H9FNFUROf3wh7SmqJp-QV30";
+   // $tokenvalido = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJNZXVzaXRlIiwiaWF0IjoxNzU2OTMwNDE1LCJleHAiOjE3NTY5MzQwMTUsInN1YiI6eyJpZCI6NSwibm9tZSI6IlBhbWVsbGEiLCJlbWFpbCI6InBhbWVsbGFwZXJldG9AZ21haWwuY29tIiwiY2FyZ28iOiJjYW1hcmVpcmEifX0.tPiz6TM3C690nlJf9iaqrmKL-92yFJQVvFK_lgtQp1k";
    //echo validateToken ($token);
-   echo var_dump(validateToken($tokenvalido));
+   //echo var_dump(validateToken($tokenvalido));
     //echo Password::generateHash($data['password']);
 //
     //$hash = '$2y$10$pa1c.sE65/hXgcfDKUIbLOBkpK56o8QG2zEJZIDyv2bhZKUd0sTEO';
